3b2e1b7d40c8e0cd083adeb652f15425c11b3148
Your Name <you@example.com>
2023-03-26 17:32:44 -0700

    edited schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,20 @@ USE company_db;
 
 CREATE TABLE department (
     id INT PRIMARY KEY,
-    name VARCAHR(30),
+    name VARCHAR(30),
 );
-
 
 CREATE TABLE role (
     id INT PRIMARY KEY,
-    title VARCAHR(30),
+    title VARCHAR(30),
     salary DECIMAL, 
     deparment_id INT, 
 );
 
 CREATE TABLE employee (
     id INT PRIMARY KEY,
-    first_name VARCAHR(30),
-    last_name VARCAHR(30),
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
 );
