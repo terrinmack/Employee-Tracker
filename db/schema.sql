e411feb9e60a96ff8e20d9d3d5a716207b7aacac
Your Name <you@example.com>
2023-03-26 17:28:48 -0700

    edited schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,21 +3,20 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCAHR(30) NOT NULL,
+    id INT PRIMARY KEY,
+    name VARCAHR(30),
 );
 
 
 CREATE TABLE role (
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    title VARCAHR(30) NOT NULL,
-    salary DECIMAL NOT NULL, 
+    id INT PRIMARY KEY,
+    title VARCAHR(30),
+    salary DECIMAL, 
     deparment_id INT, 
-    PRIMARY KEY (id),
 );
 
 CREATE TABLE employee (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY,
     first_name VARCAHR(30),
     last_name VARCAHR(30),
     role_id INT,
